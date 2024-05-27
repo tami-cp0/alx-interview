ff176e0642ec17184cdd10a1cdd1f5118dd5509a
tami-cp0 <findtamilore>
2024-05-27 23:30:37 +0100

    done

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Pascals triangle
+"""
+import math
+
+def pascal_triangle(n):
+    """
+    function that generate the values for a pascal triangle
+    """
+    values = []
+
+    if n <= 0:
+        return values
+    
+    for i in range(n):
+        row = []
+        # print(f"Row number {i}:")
+        for j in range(i + 1):
+            # print(f"[{i} combination {j}] = ({math.comb(i, j)})", end=" | ")
+            row.append(math.comb(i, j))
+        # print()
+        # print("===========")
+        values.append(row)
+
+    return values
+    
+    
+
