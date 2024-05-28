4f17cd61fb18b7858c7e1822335dd57cb36e4081
tami-cp0 <findtamilore>
2024-05-28 10:58:14 +0100

    fixed pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,6 +4,7 @@ Pascals triangle
 """
 import math
 
+
 def pascal_triangle(n):
     """
     function that generate the values for a pascal triangle
@@ -12,27 +13,14 @@ def pascal_triangle(n):
 
     if n <= 0:
         return values
-    
-    # for i in range(n):
-    #     row = []
-    #     print(f"Row number {i}:")
-    #     for j in range(i + 1):
-    #         print(f"[{i} combination {j}] = ({math.comb(i, j)})", end=" | ")
-    #         row.append(math.comb(i, j))
-    #     print()
-    #     print("===========")
-    #     values.append(row)
 
     for i in range(n):
         row = []
-        max = 2**i
+        # print(f"Row number {i}:")
         for j in range(i + 1):
-            if j > 0:
-                row.append(i + j)
+            # print(f"[{i} combination {j}] = ({math.comb(i, j)})", end=" | ")
+            row.append(math.comb(i, j))
+        # print()
+        # print("===========")
         values.append(row)
-            
-
-
-
-
     return values
