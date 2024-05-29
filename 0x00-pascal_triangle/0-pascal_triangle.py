b201f5a6f157a9739857615f63844e9913f0374d
tami-cp0 <findtamilore>
2024-05-29 20:40:57 +0100

    edited algo & comments. O(n^2) time

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -16,12 +16,8 @@ def pascal_triangle(n):
     # this solves it but uses the math library.
     # for i in range(n):
     #     row = []
-    #     print(f"Row number {i}:")
     #     for j in range(i + 1):
-    #         print(f"[{i} combination {j}] = ({math.comb(i, j)})", end=" | ")
     #         row.append(math.comb(i, j))
-    #     print()
-    #     print("===========")
     #     result.append(row)
 
     for i in range(n):
@@ -33,7 +29,7 @@ def pascal_triangle(n):
         temp = [0] + result[-1] + [0] 
         row = []
         # loop though the length of the previous row plus 1
-        for j in range(len(result[-1]) + 1):
+        for j in range(i + 1):
             # calculate the value based on the 2 numbers directly above the
             # row position
             row.append(temp[j] + temp[j + 1])
