2eff7da4de5e02541af320c355e37ec535a8ee11
tami-cp0 <findtamilore>
2024-05-29 20:35:56 +0100

    omitted libraries. O(n^2) time

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,25 +2,38 @@
 """
 Pascals triangle
 """
-import math
-
+# import math
 
 def pascal_triangle(n):
     """
-    function that generate the values for a pascal triangle
+    function that generate the result for a pascal triangle
     """
-    values = []
+    result = [[1]]
 
     if n <= 0:
-        return values
+        return result
+    
+    # this solves it but uses the math library.
+    # for i in range(n):
+    #     row = []
+    #     print(f"Row number {i}:")
+    #     for j in range(i + 1):
+    #         print(f"[{i} combination {j}] = ({math.comb(i, j)})", end=" | ")
+    #         row.append(math.comb(i, j))
+    #     print()
+    #     print("===========")
+    #     result.append(row)
 
-    for i in range(n):
+    for i in range(n - 1):
+        # create a row with 0 ... 0
+        temp = [0] + result[-1] + [0] 
         row = []
-        # print(f"Row number {i}:")
-        for j in range(i + 1):
-            # print(f"[{i} combination {j}] = ({math.comb(i, j)})", end=" | ")
-            row.append(math.comb(i, j))
-        # print()
-        # print("===========")
-        values.append(row)
-    return values
+        # loop though the length of the previous row plus 1
+        for j in range(len(result[-1]) + 1):
+            # calculate the value based on the 2 numbers directly above the
+            # row position
+            row.append(temp[j] + temp[j + 1])
+        # append complete row to result
+        result.append(row)
+
+    return result
