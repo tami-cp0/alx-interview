832c168c169b9958cd70d635502b591a8ebafbb2
tami-cp0 <findtamilore>
2024-06-17 14:30:36 +0100

    code && readme update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,47 +4,46 @@ import sys
 import re
 from typing import Tuple
 
-
-stats = {}
-
-
-def calc_stats(match: Tuple) -> None:
-    """
-    Processes and stores the stats
-    Returns:
-        None
-    """
-    global stats
-
-    stats['size'] = stats.get('size', 0) + int(match[4])
-    stats[match[3]] = stats.get(match[3], 0) + 1
-
-
-def display() -> None:
-    """
-    Displays the processed stats
-    Returns:
-        None
-    """
-    global stats
-    print(f"File size: {stats.get('size')}")
-
-    for stat in sorted(stats.keys()):
-        if stat == "size":
-            continue
-        print(f"{stat}: {stats.get(stat)}")
-
-
-def handler(signum, frame):
-    """
-    Handler for Ctrl + C SIGINT
-    Returns:
-        None
-    """
-    display()
-
-
 if __name__ == '__main__':
+
+    stats = {}
+
+
+    def calc_stats(match: Tuple) -> None:
+        """
+        Processes and stores the stats
+        Returns:
+            None
+        """
+        global stats
+
+        stats['size'] = stats.get('size', 0) + int(match[4])
+        stats[match[3]] = stats.get(match[3], 0) + 1
+
+
+    def display() -> None:
+        """
+        Displays the processed stats
+        Returns:
+            None
+        """
+        global stats
+        print(f"File size: {stats.get('size')}")
+
+        for stat in sorted(stats.keys()):
+            if stat == "size":
+                continue
+            print(f"{stat}: {stats.get(stat)}")
+
+
+    def handler(signum, frame):
+        """
+        Handler for Ctrl + C SIGINT
+        Returns:
+            None
+        """
+        display()
+
     count = 0
 
     # listens to Ctrl + C then displays the stats
