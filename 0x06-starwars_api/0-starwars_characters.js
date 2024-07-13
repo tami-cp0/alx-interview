4358e3a1b3f897ca65ec3b1a29845adf6aeb466f
tami-cp0 <findtamilore>
2024-07-13 21:46:18 +0100

    semistandard compliant

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,23 +2,21 @@
 const request = require('request');
 
 if (process.argv.length > 2) {
-  const movie_id = process.argv[2];
+  const movieId = process.argv[2];
 
-  request(`https://swapi-api.alx-tools.com/api/films/${movie_id}`, async function (error, response, body) {
+  request(`https://swapi-api.alx-tools.com/api/films/${movieId}`, async function (error, response, body) {
     if (error) {
       console.error('Error making GET request:', error);
     } else if (response.statusCode !== 200) {
       console.error('Failed to get data:', response.statusCode);
     } else {
-      const link_list = JSON.parse(body).characters;
-      await fetchNames(link_list);
+      const links = JSON.parse(body).characters;
+      await fetchNames(links);
     }
   });
-
 }
 
-async function fetchNames(links) {
-
+async function fetchNames (links) {
   for (const link of links) {
     await new Promise((resolve, reject) => {
       request(link, (err, res, body) => {
@@ -32,5 +30,4 @@ async function fetchNames(links) {
       });
     });
   }
-
 }
